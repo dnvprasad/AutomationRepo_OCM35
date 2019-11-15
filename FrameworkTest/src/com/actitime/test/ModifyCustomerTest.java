d7559bd2676b5c5ac7c98042ffc983b8f7c6cc8e
Prasad <Prasad@192.168.1.5>
2019-11-15 22:54:04 +0530

    My Second Commit

---
+++ FrameworkTest/src/com/actitime/test/ModifyCustomerTest.java
@@ -0,0 +1,16 @@
+package com.actitime.test;
+
+import org.testng.annotations.Test;
+
+public class ModifyCustomerTest {
+	
+	@Test
+	public void modifyCustomerTest()
+	{
+		System.out.println("====STEP1====");
+		System.out.println("====STEP2====");
+		System.out.println("====STEP3====");
+		System.out.println("====STEP4====");
+	}
+
+}
