ffcbd9323b0d5c6f964b30f7a6f1e3924639534a
Prasad <Prasad@192.168.1.5>
2019-11-15 23:14:52 +0530

    My Third Commit

---
+++ FrameworkTest/src/com/actitime/test/DeleteCustomerTest.java
@@ -0,0 +1,15 @@
+package com.actitime.test;
+
+import org.testng.annotations.Test;
+
+public class DeleteCustomerTest {
+
+	@Test
+	public void deleteCustTest()
+	{
+		System.out.println("====STEP5====");
+		System.out.println("====STEP6====");
+		System.out.println("====STEP7====");
+		System.out.println("====STEP8====");
+	}
+}
