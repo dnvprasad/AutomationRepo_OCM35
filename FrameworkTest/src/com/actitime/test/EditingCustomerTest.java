d8dded9c4832561b39b34e42f4ef0926aad78a0a
Prasad <Prasad@192.168.1.5>
2019-11-16 22:14:34 +0530

    editing commit by user 2

---
+++ FrameworkTest/src/com/actitime/test/EditingCustomerTest.java
@@ -0,0 +1,15 @@
+package com.actitime.test;
+
+import org.testng.annotations.Test;
+
+public class EditingCustomerTest {
+	
+	@Test
+	public void editingTest()
+	{
+		System.out.println("Step 1");
+		System.out.println("Step 2");
+		System.out.println("Stpe 4");
+	}
+
+}
