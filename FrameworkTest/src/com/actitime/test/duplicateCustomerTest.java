b261ca80752e0a9310e27e9b9365edbd98533f2a
Prasad <Prasad@192.168.1.5>
2019-11-16 20:44:48 +0530

    My Commit 4

---
+++ FrameworkTest/src/com/actitime/test/duplicateCustomerTest.java
@@ -0,0 +1,15 @@
+package com.actitime.test;
+
+import org.testng.annotations.Test;
+
+public class duplicateCustomerTest {
+	
+	@Test
+	public void duplicateTest()
+	{
+		System.out.println("Step 1");
+		System.out.println("Step 2");
+		System.out.println("Step 4");
+	}
+
+}
